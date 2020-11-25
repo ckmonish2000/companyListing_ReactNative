6f4e7ec51a3ae8ac51246b39719f2d7be463a0c3
ckmonish2000 <ckmonish2000@gmail.com>
2020-11-25 13:25:27 +0530

    touchable component

--- components/List.js
+++ components/List.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {View,ScrollView,Image,Text} from "react-native"
+import {View,ScrollView,Image,Text,TouchableOpacity} from "react-native"
 
 
 export default function List(props) {
@@ -12,17 +12,19 @@ export default function List(props) {
             {data.map((e,idx)=>{
                 if(e.company_image_url===""){console.log(e)}
                return(  
-               <View style={{flexDirection:"row",borderTopColor:"black",borderWidth:0.5}} key={e.company_id+Math.random()}>
+                   <TouchableOpacity onPress={()=>{console.log(e.company_name)}}>
+               <View style={{flexDirection:"row",borderTopColor:"grey",borderWidth:0.5}} key={e.company_id+Math.random()}>
                <Image style={{padding:20,marginLeft:10}} source={{
                     uri:e.company_image_url,
                     height:40,
-                    width:40
+                    width:20
                 }}/>
                 <View>
             <Text style={{marginLeft:20}} numberOfLines={0.5}>{e.company_name}</Text>
             
                 </View>
                 </View>
+                </TouchableOpacity>
                 )
             })}
         </ScrollView>
